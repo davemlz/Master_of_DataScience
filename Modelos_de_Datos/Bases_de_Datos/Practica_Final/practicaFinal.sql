4f6d245c16c20e2e9ade6dec2ab0795cf4ab200a
davemlz <dml.mont@gmail.com>
2019-12-11 18:09:09 +0100

    Adding Advanced Data Curation and Statistics

---
+++ Modelos_de_Datos/Bases_de_Datos/Practica_Final/practicaFinal.sql
@@ -0,0 +1,60 @@
+-- PRACTICA FINAL.
+-- MASTER EN DATA SCIENCE
+
+-- DAVID MONTERO LOAIZA
+
+-- 1. Crea una nueva tabla para almacenar las temporadas de las series.
+-- La primary key ha de ser el par de campos “idSerie, numTemporada”.
+-- La descripción de la tabla es la siguiente: (2 ptos)
+CREATE TABLE TEMPORADAS(
+
+    idserie INT,
+    numTemporada INT,    
+    fechaEstreno DATE NOT NULL,
+    fechaRegistro DATE NOT NULL,
+    disponible BOOLEAN NOT NULL,
+    
+    PRIMARY KEY(idserie,numTemporada),
+    FOREIGN KEY (idserie) REFERENCES SERIES(idSerie),    
+    CHECK(fechaRegistro > fechaEstreno),
+    CHECK(disponible IN (0,1))
+
+);
+
+-- 2. Añadir una nueva columna a la tabla "generos" para almacenar un campo denominado "descripcion" (0.25 ptos).
+ALTER TABLE GENEROS ADD descripcion CHAR;
+
+-- 3. Crea un índice sobre el par de campos “titulo” y “anyoFin” de las series (0.25 ptos)
+CREATE INDEX idx_tituloafin ON SERIES (titulo DESC,anyoFin);
+
+-- 4. Mostrar el “idserie”, “titulo”, “titulo original” y “sinopsis” de todas las series,
+-- ordenadas por título descendentemente (0.5 ptos)
+SELECT s.idSerie, s.titulo, s.tituloOriginal, s.sinopsis
+FROM SERIES s
+ORDER BY s.titulo DESC;
+
+-- 5. Retornar los datos de los usuarios franceses o noruegos (0.5 ptos)
+SELECT * FROM USUARIOS
+WHERE pais IN ('Francia','Noruega');
+
+-- 6. Mostrar los datos de los actores junto con los datos de las series en las que actúan (0.75 ptos)
+SELECT a.*, s.* FROM ACTORES a
+JOIN REPARTO r ON a.idActor = r.idActor
+JOIN SERIES s ON r.idSerie = s.idSerie;
+
+-- 7. Mostrar los datos de los usuarios que no hayan realizado nunca ninguna valoración (0.75 ptos)
+
+-- 8. Mostrar los datos de los usuarios junto con los datos de su profesión,
+-- incluyendo las profesiones que no estén asignadas a ningún usuario (0.75 ptos)
+
+-- 9. Retornar los datos de las series que estén en idioma español, y cuyo título comience por E o G (1 pto)
+
+-- 10. Retornar los “idserie”, “titulo” y “sinopsis” de todas las series junto con la puntuación media,
+-- mínima y máxima de sus valoraciones (1 pto)
+
+-- 11. Actualiza al valor 'Sin sinopsis' la sinopsis de todas las series
+-- cuya sinopsis sea nula y cuyo idioma sea el inglés (1 pto)
+
+-- 12. Utilizando funciones ventana, muestra los datos de las valoraciones
+-- junto al nombre y apellidos (concatenados) de los usuarios que las realizan,
+-- y en la misma fila, el valor medio de las puntuaciones realizadas por el usuario (1.25 ptos)
