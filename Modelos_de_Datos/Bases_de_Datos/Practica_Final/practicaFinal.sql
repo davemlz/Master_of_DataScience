786a1368ec39503a45a0bb8051fc21779e038fd6
davemlz <dml.mont@gmail.com>
2019-12-18 17:00:10 +0100

    SQL final practice finished!

--- Modelos_de_Datos/Bases_de_Datos/Practica_Final/practicaFinal.sql
+++ Modelos_de_Datos/Bases_de_Datos/Practica_Final/practicaFinal.sql
@@ -43,18 +43,40 @@ JOIN REPARTO r ON a.idActor = r.idActor
 JOIN SERIES s ON r.idSerie = s.idSerie;
 
 -- 7. Mostrar los datos de los usuarios que no hayan realizado nunca ninguna valoración (0.75 ptos)
+SELECT u.* FROM USUARIOS u
+WHERE u.idUsuario NOT IN
+(SELECT u.idUsuario FROM USUARIOS u
+NATURAL JOIN VALORACIONES v);
 
 -- 8. Mostrar los datos de los usuarios junto con los datos de su profesión,
 -- incluyendo las profesiones que no estén asignadas a ningún usuario (0.75 ptos)
+SELECT p.*, u.* FROM PROFESIONES p
+LEFT JOIN USUARIOS u ON u.idProfesion = p.idProfesion;
 
 -- 9. Retornar los datos de las series que estén en idioma español, y cuyo título comience por E o G (1 pto)
+SELECT s.* FROM SERIES s
+NATURAL JOIN IDIOMAS i
+WHERE i.idioma LIKE 'Español' AND s.titulo LIKE 'E%' or s.titulo LIKE 'G%';
 
 -- 10. Retornar los “idserie”, “titulo” y “sinopsis” de todas las series junto con la puntuación media,
 -- mínima y máxima de sus valoraciones (1 pto)
+SELECT s.idSerie, s.titulo, s.sinopsis,
+AVG(v.puntuacion) puntmedia, MIN(v.puntuacion) puntmin, MAX(v.puntuacion) puntmax
+FROM SERIES s
+NATURAL JOIN VALORACIONES v
+GROUP BY s.idSerie, s.titulo, s.sinopsis;
 
 -- 11. Actualiza al valor 'Sin sinopsis' la sinopsis de todas las series
 -- cuya sinopsis sea nula y cuyo idioma sea el inglés (1 pto)
+UPDATE SERIES SET sinopsis = 'Sin sinopsis'
+WHERE idIdioma IN
+(SELECT i.idIdioma FROM IDIOMAS i
+WHERE i.idioma = 'Inglés');
 
 -- 12. Utilizando funciones ventana, muestra los datos de las valoraciones
 -- junto al nombre y apellidos (concatenados) de los usuarios que las realizan,
 -- y en la misma fila, el valor medio de las puntuaciones realizadas por el usuario (1.25 ptos)
+SELECT v.*, u.nombre || ' ' || u.apellido1 || ' ' || u.apellido2 as nombre,
+AVG(v.puntuacion) OVER (PARTITION BY nombre) AS puntmed
+FROM USUARIOS u
+NATURAL JOIN VALORACIONES v;
